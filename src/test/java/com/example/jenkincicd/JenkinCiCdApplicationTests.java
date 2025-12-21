b65c89013e16e9e1d5bad3e48911f42335953d45
mitikud <mitikud@gmail.com>
2025-12-20 20:09:56 -0500

    fixed code to pass the test

--- src/test/java/com/example/jenkincicd/JenkinCiCdApplicationTests.java
+++ src/test/java/com/example/jenkincicd/JenkinCiCdApplicationTests.java
@@ -12,8 +12,7 @@ class JenkinCiCdApplicationTests {
 
     public static Logger logger = LoggerFactory.getLogger(JenkinCiCdApplicationTests.class);
     @Test
-//    void contextLoads() {
-         contextLoads() {
+    void contextLoads() {
         logger.info("Test case excuiting...");
         logger.info("Test case excuiting... second log statment");
         assertEquals(true, true);
